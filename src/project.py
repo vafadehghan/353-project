9e37a3d0f2686799f842269a7c98f29a0cbe9dad
vafadehghan <vafa@dehghan.ca>
2020-08-11 16:41:42 -0700

    fixed the initial df columns

--- src/project.py
+++ src/project.py
@@ -11,28 +11,29 @@ from scipy.signal import find_peaks
 
 
 def readfiles(files):
-    df = pd.DataFrame()
+    df = pd.DataFrame(columns=['aX', 'aY', 'aZ', 'aT', 'side', 'position', 'person'])
+
     for file in files:
-            fileLoc = '../data/' + file
-            temp = pd.read_csv(fileLoc, sep=',', header=0, names=[
-                            'time', 'aX', 'aY', 'aZ', 'aT'])
-            temp = temp.drop(['time'], axis=1)
+        fileLoc = '../data/' + file
+        temp = pd.read_csv(fileLoc, sep=',', header=0, names=[
+                        'time', 'aX', 'aY', 'aZ', 'aT'])
+        temp = temp.drop(['time'], axis=1)
 
-            if file[2:3] == "L":
-                temp['side'] = 'Left'
-            else:
-                temp['side'] = 'Right'
+        if file[2:3] == "L":
+            temp['side'] = 'Left'
+        else:
+            temp['side'] = 'Right'
 
-            if file[3:4] == "A":
-                temp['position'] = 'Ankle'
-            elif file[3:4] == "H":
-                temp['position'] = 'Hand'
-            elif file[3:4] == "P":
-                temp['position'] = 'Pocket'
+        if file[3:4] == "A":
+            temp['position'] = 'Ankle'
+        elif file[3:4] == "H":
+            temp['position'] = 'Hand'
+        elif file[3:4] == "P":
+            temp['position'] = 'Pocket'
 
-            if file[0:1] == "V":
-                temp['person'] = 'Vafa'
-            df = df.append(temp)
+        if file[0:1] == "V":
+            temp['person'] = 'Vafa'
+        df = df.append(temp)
             
     return df
 
