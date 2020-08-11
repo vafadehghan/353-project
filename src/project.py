22c39ee4b0ee2f836760cea6654a894ed5b78e10
vafadehghan <vafa@dehghan.ca>
2020-08-11 16:39:32 -0700

    filtered the noise out of anteroposterior acceleration

--- src/project.py
+++ src/project.py
@@ -6,40 +6,60 @@ import matplotlib.pyplot as plt
 from statsmodels.nonparametric.smoothers_lowess import lowess
 import xml.etree.ElementTree as et
 import math
+from scipy import signal
+from scipy.signal import find_peaks
+
 
 def readfiles(files):
-    df = pd.DataFrame(
-        columns=['aX', 'aY', 'aZ', 'aT', 'side', 'position', 'person'])
+    df = pd.DataFrame()
     for file in files:
-        fileLoc = '../data/' + file
-        temp = pd.read_csv(fileLoc, sep=',', header=0, names=[
-                           'time', 'aX', 'aY', 'aZ', 'aT'])
-        temp = temp.drop(['time'], axis=1)
+            fileLoc = '../data/' + file
+            temp = pd.read_csv(fileLoc, sep=',', header=0, names=[
+                            'time', 'aX', 'aY', 'aZ', 'aT'])
+            temp = temp.drop(['time'], axis=1)
 
-        if file[2:3] == "L":
-            temp['side'] = 'Left'
-        else:
-            temp['side'] = 'Right'
+            if file[2:3] == "L":
+                temp['side'] = 'Left'
+            else:
+                temp['side'] = 'Right'
 
-        if file[3:4] == "A":
-            temp['position'] = 'Ankle'
-        elif file[3:4] == "H":
-            temp['position'] = 'Hand'
-        elif file[3:4] == "P":
-            temp['position'] = 'Pocket'
+            if file[3:4] == "A":
+                temp['position'] = 'Ankle'
+            elif file[3:4] == "H":
+                temp['position'] = 'Hand'
+            elif file[3:4] == "P":
+                temp['position'] = 'Pocket'
 
-        if file[0:1] == "V":
-            temp['person'] = 'Vafa'
-        df = df.append(temp)
-
+            if file[0:1] == "V":
+                temp['person'] = 'Vafa'
+            df = df.append(temp)
+            
     return df
+
 
 def main():
     files = ['V-LA-Lin.csv', 'V-RA-Lin.csv', 'V-LP-Lin.csv',
-         'V-RP-Lin.csv', 'V-LH-Lin.csv', 'V-RH-Lin.csv']
+             'V-RP-Lin.csv', 'V-LH-Lin.csv', 'V-RH-Lin.csv']
     df = readfiles(files)
-    print(df)
+    # print(df)
+
+    # print(df[(df['position'] == 'Ankle') & (df['side'] == 'Left')])
+    la = df[(df['position'] == 'Ankle') & (df['side'] == 'Left')] # 22 steps
+    lax = la['aX'] 
+    print(lax)    
 
 
+    # b, a = signal.butter(2, 0.02, btype='lowpass', analog=False) The best one so far
+    b, a = signal.butter(2, 0.02, btype='lowpass', analog=False)
+    low_passed = signal.filtfilt(b, a, lax) 
+
+    peaks, _ = find_peaks(low_passed, height=0)
+
+    # plt.plot(lax)     
+    plt.plot(low_passed) 
+    plt.plot(peaks, low_passed[peaks], "x")
+    plt.show() 
+
+    # plt.savefig('filtered.png')
 if __name__ == '__main__':
     main()
