86abac4f5d7cda9b7b3e745db0598520bbc6bf69
vafadehghan <vafa@dehghan.ca>
2020-08-12 13:25:03 -0700

    added fft, butterworth filter, and reorganized the data

--- src/project.py
+++ src/project.py
@@ -1,76 +1,163 @@
-import sys
-import pykalman as pk
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from statsmodels.nonparametric.smoothers_lowess import lowess
-import xml.etree.ElementTree as et
-import math
-from scipy import signal
-from scipy.signal import find_peaks
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from scipy import signal, stats 
+from math import sqrt
+from numpy.fft import fft, fftshift
 
+def read_csv(path):
+    path = path + '.csv'
+    df = pd.read_csv(path, header=0, names=['time', 'ax', 'ay', 'az'])                                                  
+    df = df[(df['time'] >= 2) & (df['time'] <= 8)] # Limit the data size as not every data is of the same size
+                                                   # Exclude the first 3 seconds 
+    df['aT'] = np.sqrt(df['ax']**2 + df['ay']**2 + df['az']**2)  #Total acceleration   
+    return df     
 
-def readfiles(files):
-    df = pd.DataFrame(
-        columns=['aX', 'aY', 'aZ', 'aT', 'velocity', 'position', 'side', 'placement', 'person'])
-    for file in files:
-        fileLoc = '../data/' + file
-        temp = pd.read_csv(fileLoc, sep=',', header=0, names=[
-                           'time', 'aX', 'aY', 'aZ', 'aT'])
-        # temp = temp.drop(['time'], axis=1)
-        temp['time_shift'] = temp['time'].shift(1)
-        temp['velocity'] = (temp['time'] - temp['time_shift']) * temp['aX']
-
-        temp['position'] = temp['velocity'] * \
-            (temp['time'] - temp['time_shift'])
-        temp = temp.drop(['time'], axis=1)
-        temp = temp.drop(['time_shift'], axis=1)
-
-        if file[2:3] == "L":
-            temp['side'] = 'Left'
-        else:
-            temp['side'] = 'Right'
-
-        if file[3:4] == "A":
-            temp['placement'] = 'Ankle'
-        elif file[3:4] == "H":
-            temp['placement'] = 'Hand'
-        elif file[3:4] == "P":
-            temp['placement'] = 'Pocket'
-        if file[0:1] == "V":
-            temp['person'] = 'Vafa'
-        df = df.append(temp)
-    df = df.dropna()
-    return df
-
-
-def main():
-    files = ['V-LA-Lin.csv', 'V-RA-Lin.csv', 'V-LP-Lin.csv',
-             'V-RP-Lin.csv', 'V-LH-Lin.csv', 'V-RH-Lin.csv']
-    df = readfiles(files)
-    # print(df)
-
-    print(df[(df['placement'] == 'Ankle') & (df['side'] == 'Right')])
-    la = df[(df['placement'] == 'Ankle') & (df['side'] == 'Right')]  # 22 steps
-    # lax = la['aX']
-    print(la)
-
-    # # b, a = signal.butter(2, 0.02, btype='lowpass', analog=False) The best one so far
-    # b, a = signal.butter(2, 0.02, btype='lowpass', analog=False)
-    # low_passed = signal.filtfilt(b, a, lax)
-
-    # peaks, _ = find_peaks(low_passed, height=0)
-
-    # fft = np.fft.fft(low_passed)
-    # print(fft)
-    # plt.plot(fft)
-    # plt.show()
-
-
+# Butterworth Filter
+def BW_filter(data):
+    b, a = signal.butter(2, 0.02, btype = 'lowpass', analog = False) # The best one so far
+    low_passed = signal.filtfilt(b, a, data)
+    
     # plt.plot(lax)
     # plt.plot(low_passed)
     # plt.plot(peaks, low_passed[peaks], "x")
     # plt.show()
     # plt.savefig('filtered.png')
-if __name__ == '__main__':
+
+
+    return low_passed
+
+# First filter the data using Butterworth filter and using fft to transform the data
+def fourier_transformation(data):
+    # Filtering using Butterworth filter
+    filtered = data.apply(BW_filter)
+    filtered = filtered.reset_index(drop = True)
+    
+    # Using Fourier Transformation on the filtered data
+    # fft = np.fft.fft(data)
+    # print(fft)
+    # plt.plot(fft)
+    # plt.show()
+
+    data = data.reset_index(drop=True)
+    fourierT = filtered.apply(fft)      
+    fourierT = fourierT.apply(fftshift)
+    fourierT = fourierT.abs()
+    
+    # Calculating the sampling frequency and the average steps per second
+    length = len(data)
+    sampling_freq = round(length / data.at[length-1, 'time']) #samples per second
+    fourierT['freq'] = np.linspace(-sampling_freq/2, sampling_freq/2, num = len(data))
+    temp = fourierT[fourierT['freq'] > 0]
+    peak_index = temp['aT'].nlargest(n = 1).idxmax()
+    average = fourierT.at[peak_index , 'freq']
+    peak_val_index = fourierT['aT'].nlargest(n = 1).idxmax()
+    fourierT.at[peak_val_index, 'aT'] = temp['aT'].max()
+    
+    return fourierT , average
+
+def calc_summary(data):
+    # The parameter will be the original dataFrame after some data cleaning
+    stats = []
+    ax_summary = data['ax'].describe()      
+    ay_summary = data['ay'].describe()
+    az_summary = data['az'].describe()
+    aT_summary = data['aT'].describe()
+    for i in range(1 , 8):
+        stats.append(ax_summary[i])  
+    for i in range(1 , 8):
+        stats.append(ay_summary[i])
+    for i in range(1 , 8):
+        stats.append(az_summary[i]) 
+    for i in range(1 , 8):
+        stats.append(aT_summary[i]) 
+    return stats
+
+def BW_stats(data):
+    filtered = filtered = data.apply(BW_filter)
+    stats = calc_summary(filtered)
+    return stats
+
+def summarize():
+    summaries_person = []
+    summaries_placement = []
+
+         
+    for i in range(1 , 13):
+        df = read_csv('../data/pocket/pocket_' + str(i))
+        summary = []
+        if i <= 6:     
+            summary.append('Song-Pocket')
+        elif i > 6:
+            summary.append('Vafa-Pocket')
+        summary += BW_stats(df)
+        summary.append(fourier_transformation(df)[1])
+        summaries_person.append(summary)
+
+    for i in range(1 , 13):
+        df = read_csv('../data/hand/hand_' + str(i))
+        summary = []
+        if i <= 6:     
+            summary.append('Song-Hand')
+        else:
+            summary.append('Vafa-Hand')
+        summary += BW_stats(df)
+        summary.append(fourier_transformation(df)[1])  
+        summaries_person.append(summary)
+
+    for i in range(1 , 13):
+        df = read_csv('../data/ankle/ankle_' + str(i))
+        summary = []
+        if i <= 6:     
+            summary.append('Song-Ankle')
+        else:
+            summary.append('Vafa-Ankle')
+        summary += BW_stats(df)
+        summary.append(fourier_transformation(df)[1])  
+        summaries_person.append(summary)
+
+    for i in range(1 , 13):
+        df = read_csv('../data/pocket/pocket_' + str(i))
+        summary = []
+        summary.append('pocket')
+        summary += BW_stats(df)
+        summary.append(fourier_transformation(df)[1])     
+        summaries_placement.append(summary)
+
+    for i in range(1 , 13):
+        df = read_csv('../data/hand/hand_' + str(i))
+        summary = []
+        summary.append('hand')
+        summary += BW_stats(df)
+        summary.append(fourier_transformation(df)[1])  
+        summaries_placement.append(summary)
+
+    for i in range(1 , 13):
+        df = read_csv('../data/ankle/ankle_' + str(i))
+        summary = []
+        summary.append('ankle')
+        summary += BW_stats(df)
+        summary.append(fourier_transformation(df)[1])  
+        summaries_placement.append(summary)
+        
+    df = pd.DataFrame(summaries_person , columns = ['class', 
+    'mean(ax)' , 'std(ax)' , 'min(ax)' , '25th(ax)' , '50th(ax)' , '75th(ax)' , 'max(ax)',
+    'mean(ay)' , 'std(ay)' , 'min(ay)' , '25th(ay)' , '50th(ay)' , '75th(ay)' , 'max(ay)',
+    'mean(az)' , 'std(az)' , 'min(az)' , '25th(az)' , '50th(az)' , '75th(az)' , 'max(az)',
+    'mean(aT)' , 'std(aT)' , 'min(aT)' , '25th(aT)' , '50th(aT)' , '75th(aT)' , 'max(aT)',
+    'mean(freq)'])
+    df.to_csv('summary_person.csv' , index = False)
+
+    df2 = pd.DataFrame(summaries_placement , columns = ['class', 
+    'mean(ax)' , 'std(ax)' , 'min(ax)' , '25th(ax)' , '50th(ax)' , '75th(ax)' , 'max(ax)',
+    'mean(ay)' , 'std(ay)' , 'min(ay)' , '25th(ay)' , '50th(ay)' , '75th(ay)' , 'max(ay)',
+    'mean(az)' , 'std(az)' , 'min(az)' , '25th(az)' , '50th(az)' , '75th(az)' , 'max(az)',
+    'mean(aT)' , 'std(aT)' , 'min(aT)' , '25th(aT)' , '50th(aT)' , '75th(aT)' , 'max(aT)',
+    'mean(freq)'])
+    df2.to_csv('summary_placement.csv', index=False)
+
+def main():
+    summarize()
+
+if __name__ == "__main__":
     main()
