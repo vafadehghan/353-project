821288034c1f66f92c3aaa5ee00fdfbbf4e29c1c
vafadehghan <vafa@dehghan.ca>
2020-08-11 18:03:53 -0700

    added velocity and position to the dataframe

--- src/project.py
+++ src/project.py
@@ -11,13 +11,20 @@ from scipy.signal import find_peaks
 
 
 def readfiles(files):
-    df = pd.DataFrame(columns=['aX', 'aY', 'aZ', 'aT', 'side', 'position', 'person'])
-
+    df = pd.DataFrame(
+        columns=['aX', 'aY', 'aZ', 'aT', 'velocity', 'position', 'side', 'placement', 'person'])
     for file in files:
         fileLoc = '../data/' + file
         temp = pd.read_csv(fileLoc, sep=',', header=0, names=[
-                        'time', 'aX', 'aY', 'aZ', 'aT'])
+                           'time', 'aX', 'aY', 'aZ', 'aT'])
+        # temp = temp.drop(['time'], axis=1)
+        temp['time_shift'] = temp['time'].shift(1)
+        temp['velocity'] = (temp['time'] - temp['time_shift']) * temp['aX']
+
+        temp['position'] = temp['velocity'] * \
+            (temp['time'] - temp['time_shift'])
         temp = temp.drop(['time'], axis=1)
+        temp = temp.drop(['time_shift'], axis=1)
 
         if file[2:3] == "L":
             temp['side'] = 'Left'
@@ -25,16 +32,15 @@ def readfiles(files):
             temp['side'] = 'Right'
 
         if file[3:4] == "A":
-            temp['position'] = 'Ankle'
+            temp['placement'] = 'Ankle'
         elif file[3:4] == "H":
-            temp['position'] = 'Hand'
+            temp['placement'] = 'Hand'
         elif file[3:4] == "P":
-            temp['position'] = 'Pocket'
-
+            temp['placement'] = 'Pocket'
         if file[0:1] == "V":
             temp['person'] = 'Vafa'
         df = df.append(temp)
-            
+    df = df.dropna()
     return df
 
 
@@ -44,23 +50,27 @@ def main():
     df = readfiles(files)
     # print(df)
 
-    # print(df[(df['position'] == 'Ankle') & (df['side'] == 'Left')])
-    la = df[(df['position'] == 'Ankle') & (df['side'] == 'Left')] # 22 steps
-    lax = la['aX'] 
-    print(lax)    
+    print(df[(df['placement'] == 'Ankle') & (df['side'] == 'Right')])
+    la = df[(df['placement'] == 'Ankle') & (df['side'] == 'Right')]  # 22 steps
+    # lax = la['aX']
+    print(la)
+
+    # # b, a = signal.butter(2, 0.02, btype='lowpass', analog=False) The best one so far
+    # b, a = signal.butter(2, 0.02, btype='lowpass', analog=False)
+    # low_passed = signal.filtfilt(b, a, lax)
+
+    # peaks, _ = find_peaks(low_passed, height=0)
+
+    # fft = np.fft.fft(low_passed)
+    # print(fft)
+    # plt.plot(fft)
+    # plt.show()
 
 
-    # b, a = signal.butter(2, 0.02, btype='lowpass', analog=False) The best one so far
-    b, a = signal.butter(2, 0.02, btype='lowpass', analog=False)
-    low_passed = signal.filtfilt(b, a, lax) 
-
-    peaks, _ = find_peaks(low_passed, height=0)
-
-    # plt.plot(lax)     
-    plt.plot(low_passed) 
-    plt.plot(peaks, low_passed[peaks], "x")
-    plt.show() 
-
+    # plt.plot(lax)
+    # plt.plot(low_passed)
+    # plt.plot(peaks, low_passed[peaks], "x")
+    # plt.show()
     # plt.savefig('filtered.png')
 if __name__ == '__main__':
     main()
